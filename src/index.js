6d4caf97fe3b4355a9a0f99d7989fa92c19a2417
halonso14 <hdsbsb@naver.com>
2022-01-05 11:33:10 +0900

    fix: UPDATE load dotenv from command line

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
-import dotenv from 'dotenv';
 import personSchema from './schema';
 import personResolver from './resolver';
 import dbConnect from './models';
-
-dotenv.config();
 dbConnect();
 
 const app = express();
